75d037e4aef4e9f52feb54479195b9070e316c84
nitish <nitish.hardeniya@happay.in>
2019-09-04 12:03:12 +0530

    Updates

---
+++ precache-manifest.867f04824fbdc5f30f5609e8377a31de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be3bec4d9dd9b196437f9aa0072eaae2",
+    "url": "/vg-shop/index.html"
+  },
+  {
+    "revision": "6dc71aa40fc403042193",
+    "url": "/vg-shop/static/css/main.4673b234.chunk.css"
+  },
+  {
+    "revision": "f0037875dcb527f25744",
+    "url": "/vg-shop/static/js/2.0b553197.chunk.js"
+  },
+  {
+    "revision": "6dc71aa40fc403042193",
+    "url": "/vg-shop/static/js/main.99f71fc8.chunk.js"
+  },
+  {
+    "revision": "be12963448048dabdba1",
+    "url": "/vg-shop/static/js/runtime~main.addd0b7a.js"
+  }
+]);
